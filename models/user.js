251aa3c97b08597d59d4d1f5dee37f975b42f4fd
Nathan Rydel <nathanrydel@gmail.com>
2024-01-08 20:51:51 -0700

    removed debug statement logs

--- models/user.js
+++ models/user.js
@@ -77,7 +77,6 @@ class User {
    *          last_login_at } */
 
   static async get(username) {
-    console.log("running User.get", username);
     const result = await db.query(`
       SELECT username,
              first_name,
@@ -89,7 +88,6 @@ class User {
         WHERE username = $1`,
       [username]);
     const user = result.rows[0];
-    console.log("user in User.get: ", user);
 
     if (!user) throw new NotFoundError(`No such user: ${username}`);
 
