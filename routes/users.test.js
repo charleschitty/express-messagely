2a91d662f9a345945c7eb6b950e313717179833d
Charles Chitty <charlescchitty@gmail.com>
2024-01-09 11:18:40 -0800

    Added test message instances in our test User Message routes

--- routes/users.test.js
+++ routes/users.test.js
@@ -111,6 +111,51 @@ describe("User Routes tests", function () {
     });
   });
 
+describe("User Message Routes tests", function () {
+  let testUser1Token;
+  let testUser2Token;
+  let user1;
+  let user2;
+  let m1;
+  let m2;
+
+  beforeEach(async function () {
+    await db.query("DELETE FROM messages");
+    await db.query("DELETE FROM users");
+
+    user1 = await User.register({
+      username: "test1",
+      password: "password",
+      first_name: "Test1",
+      last_name: "Testy1",
+      phone: "+14155550000",
+    });
+
+    user2 = await User.register({
+      username: "test2",
+      password: "password",
+      first_name: "Test2",
+      last_name: "McTest2",
+      phone: "+14155551234",
+    });
+
+    testUser1Token = jwt.sign({ username: "test1" }, SECRET_KEY);
+    testUser2Token = jwt.sign({ username: user2.username }, SECRET_KEY);
+
+    m1 = await Message.create({
+      from_username: "test1",
+      to_username: "test2",
+      body: "u1-to-u2",
+    });
+
+    m2 = await Message.create({
+      from_username: "test2",
+      to_username: "test1",
+      body: "u2-to-u1",
+    });
+  });
+});
+
 afterAll(async function () {
   await db.end();
 });
